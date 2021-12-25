82b825d3c20c3bde5ce52695f109ec0ee42a034f
est2mzd <est2mzd@gmail.com>
2021-12-26 07:37:11 +0900

    OK : Lidar 3-8

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -36,6 +36,7 @@ struct KdTree
 		delete root;
 	}
 
+    //--------------------------------------------------------------------------------------//
     // Lesson : Lidar-3-6 : Insert Points
     void insertHelper(Node** node, uint depth, std::vector<float> points, int id)
     {
@@ -65,18 +66,66 @@ struct KdTree
         //std::cout << "id = " << id << std::endl;
     }
 
+    //--------------------------------------------------------------------------------------//
+    // Lesson : Lidar-3-6 : Insert Points
 	void insert(std::vector<float> points, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root
         insertHelper(&root, 0, points, id);
-
 	}
 
+    //--------------------------------------------------------------------------------------//
+    // Lesson : Lidar-3-7 : Searching Points in a KD-Tree
+    void searchHelper(std::vector<float> target, Node* node, int depth, float distanceTol, std::vector<int>& ids)
+    {
+
+        if(node != nullptr)
+        {
+            // Check if node is in the box of target torelance
+            bool flagXMin = node->point[0] >= (target[0]-distanceTol);
+            bool flagXMax = node->point[0] <= (target[0]+distanceTol);
+            bool flagYMin = node->point[1] >= (target[1]-distanceTol);
+            bool flagYMax = node->point[1] <= (target[1]+distanceTol);
+            //
+            if( flagXMin && flagXMax && flagYMin && flagYMax )
+            {
+                float dx = node->point[0] - target[0];
+                float dy = node->point[1] - target[1];
+                float distance = sqrt( dx*dx + dy*dy);
+
+                if(distance <= distanceTol)
+                {
+                    ids.push_back(node->id);
+                    std::cout << "id = " << node->id << " / distance = " << distance << std::endl;
+                }
+            }
+
+            // recursively do searchHelper
+            // Check : we must search in which area
+            int XorY = depth % 2;
+            bool selectLowerArea  = (target[XorY] - distanceTol) < node->point[XorY];
+            bool selectHigherArea = (target[XorY] + distanceTol) > node->point[XorY];
+            if( selectLowerArea )
+            {
+                searchHelper(target, node->left, depth+1, distanceTol, ids);
+            }
+            if( selectHigherArea )
+            {
+                searchHelper(target, node->right, depth+1, distanceTol, ids);
+            }
+        }
+    }
+
+
+    //--------------------------------------------------------------------------------------//
+    // Lesson : Lidar-3-7 : Searching Points in a KD-Tree
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+        // Helper function
+        searchHelper(target, root, 0, distanceTol, ids);
 		return ids;
 	}
 	
