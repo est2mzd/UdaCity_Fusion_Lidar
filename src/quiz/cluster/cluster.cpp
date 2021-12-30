d64c64d158ad5fa927e1c4c97558f24c858f4915
est2mzd <est2mzd@gmail.com>
2021-12-30 11:59:45 +0900

    auto backup from SurfacePro7

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -80,15 +80,53 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterHelper(int id, const std::vector<std::vector<float>> poitns, std::vector<int>& cluster, std::vector<bool>& processed, KdTree* tree, float distanceTol)
+{
+	// process the point of "input id"
+	processed[id] = true;
+	clusters.puch_back(id);
+
+	// Find nearest point list of points[id] --> nearest
+	std::vector<int> nearest = tree->search(points[id], distanceTol);
+
+	// process the points in "ids of nearest"
+	for(int i : nearest)
+	{
+		if(!processed[i])
+		{
+			clusterHelper(i, points, cluster, processed, tree, distanceTol);
+		}
+	}
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
-
+	//--------------------------------------------------------------------------------------//
+	// Lesson : Lidar-3-8 : Euclidean Clustering 
 	// TODO: Fill out this function to return list of indices for each cluster
-
 	std::vector<std::vector<int>> clusters;
  
-	return clusters;
+	// init processed with "false". The size of Array is points.size()
+	std::vector<bool> processed(points.size(), false);
 
+	int i=0;
+	while(i < points.size())
+	{
+		if(processed[i])
+		{
+			i++;
+			continue;
+		}
+
+		std::vector<int> cluster;
+		clusterHelper(i, points, cluster, processed, tree, distanceTol);
+		clusters.push_back(cluster);
+		i++;
+	}
+
+
+
+	return clusters;
 }
 
 int main ()
@@ -117,16 +155,22 @@ int main ()
   	int it = 0;
   	render2DTree(tree->root,viewer,window, it);
   
+     //--------------------------------------------------------------------------------------//
+    // Lesson : Lidar-3-7 : Searching Points in a KD-Tree 
   	std::cout << "Test Search" << std::endl;
   	std::vector<int> nearby = tree->search({-6,7},3.0);
   	for(int index : nearby)
       std::cout << index << ",";
   	std::cout << std::endl;
+	//--------------------------------------------------------------------------------------//
 
   	// Time segmentation process
   	auto startTime = std::chrono::steady_clock::now();
-  	//
+  	
+     //--------------------------------------------------------------------------------------//
+    // Lesson : Lidar-3-8 : Euclidean Clustering 
   	std::vector<std::vector<int>> clusters = euclideanCluster(points, tree, 3.0);
+	//--------------------------------------------------------------------------------------//  
   	//
   	auto endTime = std::chrono::steady_clock::now();
   	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
