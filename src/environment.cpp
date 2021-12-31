fb02d73d2b521a0a33d6adbd32a4b2adf60c812d
est2mzd <est2mzd@gmail.com>
2021-12-31 17:20:00 +0900

    auto backup SurfacePro7

--- src/environment.cpp
+++ src/environment.cpp
@@ -143,8 +143,8 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
     //-----------------------------------------------------------------------------------------
     // Step-4 : Euclidean Clustering with PCL
     float clusterTolerance = 1.0;
-    int minSize = 3;
-    int maxSize = 300000;
+    int minSize = 100;
+    int maxSize = 900;
     std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, clusterTolerance, minSize, maxSize);
 
     int clusterId = 0;
