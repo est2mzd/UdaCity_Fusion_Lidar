5bdd4cf65afa95e889638dc16f68a21d0f455123
est2mzd <est2mzd@gmail.com>
2022-01-02 13:57:02 +0900

    auto backup SurfacePro7

--- src/environment.cpp
+++ src/environment.cpp
@@ -213,14 +213,14 @@ int main (int argc, char** argv)
     std::string folderPath  = "/home/workspace/Udacity_Fusion_Lidar";
     std::string fileNamePng = folderPath + "/png/L4_6_" + getCurrentTime("") + ".png";
     std:cout << fileNamePng << std::endl;
-    pcl::visualization::PCLVisualizer* pngObj("PNG OBJ");
+    pcl::visualization::PCLVisualizer pngObj("PNG OBJ");
     
 
     while (!viewer->wasStopped ())
     {
         viewer->spinOnce ();
 
-        pngObj->saveScreenshot(fileNamePng);
+        pngObj.saveScreenshot(fileNamePng);
         break;
     } 
 
